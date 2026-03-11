37ffdaa082115bf1c641a0eda73f26ac7fa73b82
Klein Moretti <169626976+XiaoXioe@users.noreply.github.com>
2026-03-11 18:53:58 +0700

    feat: add uabea with dotnet wrapper fix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Koleksi custom Nix packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system} = {
+        uabea = pkgs.callPackage ./pkgs/uabea/default.nix { };
+
+      };
+    };
+}
