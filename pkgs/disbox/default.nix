79c52a2b8a29b1be30ea1ead90f71db370cf56b9
Klein Moretti (admin) <169626976+XiaoXioe@users.noreply.github.com>
2026-03-21 20:01:01 +0700

    chore: update flake.lock and bump disbox to 3.8.0

--- pkgs/disbox/default.nix
+++ pkgs/disbox/default.nix
@@ -6,12 +6,12 @@
 
 let
   pname = "disbox";
-  version = "3.7.5";
+  version = "3.8.0";
 
   src = fetchurl {
     url = "https://github.com/naufal-backup/disbox/releases/download/v${version}/Disbox-Linux-x64.AppImage";
 
-    sha256 = "sha256:9d37b3df768e97c214f128f33b3ac58d7cb34fb701a92a747779d8a44b6ff973";
+    sha256 = "sha256:27a2d8d8adc66797376b30f5ce3b58e9e6fd3a55a47b24947ba72ab8bab8f483";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
@@ -20,11 +20,27 @@ appimageTools.wrapType2 {
   inherit pname version src;
 
   # Mengekstrak ikon dan file .desktop agar Disbox muncul di menu aplikasi KDE Plasma / Cinnamon
+  # extraInstallCommands = ''
+  #   install -m 444 -D ${appimageContents}/disbox.desktop -t $out/share/applications
+  #   cp -r ${appimageContents}/usr/share/icons/hicolor/0x0/apps $out/share
+  #   substituteInPlace $out/share/applications/disbox.desktop \
+  #     --replace 'Exec=AppRun' 'Exec=${pname}'
+  # '';
   extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/disbox.desktop -t $out/share/applications
-    cp -r ${appimageContents}/usr/share/icons $out/share
+    # Kita ambil dari folder 0x0 yang aneh itu,
+    # tapi kita simpan ke folder 512x512 agar sistem bisa membacanya.
+    # Flag -D akan membuatkan folder share/icons/hicolor/512x512/apps/ secara otomatis.
+
+    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/0x0/apps/disbox.png \
+      $out/share/icons/hicolor/512x512/apps/disbox.png
+
+    # Install file desktop
+    install -m 444 -D ${appimageContents}/disbox.desktop $out/share/applications/disbox.desktop
+
+    # Pastikan file desktop merujuk ke nama ikon yang benar
     substituteInPlace $out/share/applications/disbox.desktop \
-      --replace 'Exec=AppRun' 'Exec=${pname}'
+      --replace 'Exec=AppRun' 'Exec=${pname}' \
+      --replace 'Icon=disbox' 'Icon=disbox'
   '';
 
   meta = with lib; {
